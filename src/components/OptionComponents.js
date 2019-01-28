9195c3911d4f6d581e2bf20d6b4992fc64834309
daxbjj <salina.dario@gmail.com>
2019-01-28 16:29:09 +0100

    added container and components

--- src/components/OptionComponents.js
+++ src/components/OptionComponents.js
@@ -1,5 +1,14 @@
 import React, { Component} from 'react'
 
 class OptionComponents extends Components{
-    
+    render(){
+
+        return(
+            <div>
+                <p>German shepard</p>
+            </div>
+        )
+
+
+    }
 }
--- src/components/OptionContainer.js
+++ src/components/OptionContainer.js
@@ -2,12 +2,25 @@ import React, { Component} from 'react'
 import OptionComponents from './OptionComponents'
 
 
+const data = {
+
+}
+
+
+
+
 class OptionContainer extends Components{
     render(){
+
+
+
+
+
         return (
             <div>
                 <OptionComponents />
-            
+                <OptionComponents />
+                <OptionComponents />
             </div>
         )
 
