0e06f0a34219c4d8c2b82bf84836359ade5b1578
daxbjj <salina.dario@gmail.com>
2019-01-28 16:18:41 +0100

    created gamecomponents and container

---
+++ src/components/OptionContainer.js
@@ -0,0 +1,16 @@
+import React, { Component} from 'react'
+import OptionComponents from './OptionComponents'
+
+
+class OptionContainer extends Components{
+    render(){
+        return (
+            <div>
+                <OptionComponents />
+            
+            </div>
+        )
+
+        
+    }
+}
