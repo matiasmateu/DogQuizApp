2aec16634e90a4f2538a3527137a07f579dfeddb
MATIAS MATEU <matias_mateu@icloud.com>
2019-01-28 16:08:05 +0100

    Added Render Method to Game Container

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -1,7 +1,14 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
+
 
 class GameContainer extends Component{
-
+    render(){ 
+        return (
+        <header>
+           asdasds
+        </header>
+        
+    )}
 }
 
 export default GameContainer
