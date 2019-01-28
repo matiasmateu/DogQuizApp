0c99a4d935d09ca9e0ba5abf933ceea120bb151a
Mayron van Leenden <mvanleenden@gmail.com>
2019-01-28 16:25:17 +0100

    Fetching breed list from API

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 
 class GameContainer extends Component{
+
+    state = {
+        breeds: null
+    }
+
+    componentDidMount() {
+        axios.get('https://dog.ceo/api/breeds/list/all').then((result) => {
+            this.setState(Object.keys(result.data.message))
+        })
+    }
+
     render(){ 
         return (
         <header>
