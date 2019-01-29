92638367c7482f4a899e54b46f21e4416279e5bc
Mayron van Leenden <mvanleenden@gmail.com>
2019-01-29 15:16:04 +0100

    simple next button implemented

--- src/components/GameContainer/Game.js
+++ src/components/GameContainer/Game.js
@@ -9,7 +9,9 @@ export default (props) => {
   return (
     <section className="game">
         <HeaderContainer />
-        <CardPicture dogPicture='https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia-cdn.tripadvisor.com%2Fmedia%2Fphoto-s%2F08%2F16%2Fc2%2F53%2Fbonita-springs-dog-park.jpg&f=1'/>
+
+        <CardPicture dogPicture={props.question}/>
+        <button onClick={props.handleNextQuestion}>Next Question</button>
         <OptionContainer />
     </section> 
   )
