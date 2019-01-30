311be84a57fb5abaa111f564e9b7fb0a304d0c17
Mayron van Leenden <mvanleenden@gmail.com>
2019-01-30 10:11:33 +0100

    Finished with Header styling + resonsiveness

--- src/components/Header/ScoreBoard.js
+++ src/components/Header/ScoreBoard.js
@@ -8,8 +8,8 @@ class ScoreBoard extends Component{
         return (
         <div className="score-board">
             <ul>
-                <li>Score:{this.props.stats.score}</li>
-                <li>Level:{this.props.stats.level}</li>
+                <li>Score: {this.props.stats.score}</li>
+                <li>Level: {this.props.stats.level}</li>
                 <li>
                     <WinnerCount total={this.props.stats.counter}/>
                 </li>
