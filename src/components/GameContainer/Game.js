0c0800b33603b12f68c96d89565ec95e12389459
Dario <salina.dario@gmail.com>
2019-01-31 16:54:10 +0100

    previous change

--- src/components/GameContainer/Game.js
+++ src/components/GameContainer/Game.js
@@ -3,32 +3,12 @@ import CardPicture from './CardPicture'
 import HeaderContainer from '../Header/Index'
 import OptionContainer from '../Options/Index'
 import Modal from '../Modal';
-import InfoContainer from '../Info/InfoContainer';
-
-
-export default (props) => {
-  const { currentQuestion } = props
-  if(currentQuestion) {
-    return (
-      <section className="game">
-          <HeaderContainer />
-          <CardPicture type={currentQuestion.type} question={currentQuestion.question}/>
-          <OptionContainer />
-          <InfoContainer />
-          <Modal/>
-      </section> 
-    )
-  }
-
-  return (<div>Loading</div>)
-  
-}
 
 
 // You can see a picture of a dog, try to guess the breed among the three choices. 
 // After three right guesses you will go to the next level.
 //  
-=======
+
 import Loader from '../Loader';
 import './game.css'
 
