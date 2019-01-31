f5c1eb632f2fc930c69185dc9ccb605999e30500
Matias <Matias_mateu@icloud.com>
2019-01-31 17:57:03 +0100

    Add KeyPressed to Images

--- src/components/Options/OptionImageComponent.js
+++ src/components/Options/OptionImageComponent.js
@@ -5,9 +5,14 @@ import './OptionContainer.css'
 class OptionImageComponent extends Component{
     render(){
         return(
-            <div onClick={this.props.onClick} className="optionImageComponent__image">  
-                <img src={this.props.breed} alt="breed-option"></img>
-            </div>
+            
+                <div onClick={this.props.onClick} className="optionImageComponent__image"> 
+                    <img src={this.props.breed} alt="breed-option"></img>
+                    <span className="index-images">{this.props.index+1}</span>
+                </div>
+                
+            
+            
         )
     }
 }
